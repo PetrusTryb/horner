7fd79c4c77d39564ef7726226f4e975afc42d738
Piotr Trybisz <ptrybisz@gmail.com>
2020-09-08 22:01:38 +0200

    Added header

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 function regenerateInput(){
     let level=$("#lvl").val()
-    console.log(level)
     $("#input").html("")
     for(let i=level; i>=0; --i){
         let ihtml=`<div class="field is-horizontal">
@@ -12,7 +11,7 @@ function regenerateInput(){
 </div>
 </div>
 <div class="field-label is-normal">
-<label class="label">${i>0?`x<sup>${i}</sup>+`:``}</label>
+<label class="label">x<sup>${i}</sup>${i>0?`+`:``}</label>
 </div>
 </div>`
         $("#input").append(ihtml)
@@ -28,7 +27,6 @@ function calculate(){
         arr.push(Number(numbers[n].value))
         $("#row1").append(`<td>${numbers[n].value}</td>`)
     })
-    console.log(arr)
     let res=arr[0]
     $("#row3").append(`<td>${res}</td>`)
     for(let i=1;i<=$("#lvl").val();i++){
